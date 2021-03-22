cce8b95936048f7c80c4ccfcaabde11a625e7060
kherma <kamil.herma01@gmail.com>
2021-03-22 18:51:02 +0100

    add middleware

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 // Exports
 // ============
 const express = require("express");
+const path = require("path");
 
 // ============
 // Setup
 // ============
 const port = 8000;
 const app = express();
+
+// ============
+// Middleware
+// ============
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // ============
 // Start Server
