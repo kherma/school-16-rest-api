a0a52b9c3e76dff4dd2766f8d35f64da61199b64
kherma <kamil.herma01@gmail.com>
2021-03-23 17:03:09 +0100

    Prepare bundle for deploy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const testimonials = require("./routers/testimonials.routes");
 const concetrs = require("./routers/concerts.routes");
 const seats = require("./routers/seats.routes");
+const path = require("path");
 const cors = require("cors");
 
 // ============
@@ -21,22 +22,27 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.use("/api", testimonials);
+app.use("/api", concetrs);
+app.use("/api", seats);
+app.use("/api/*", (req, res) => {
+  res.status(404).send({ message: "Error 404, not found" });
+});
 
 // ============
 // Endpoints
 // ============
 
-app.use("/api", testimonials);
-app.use("/api", concetrs);
-app.use("/api", seats);
-app.use((req, res) => {
-  res.status(404).send({ message: "Error 404, not found" });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 // ============
 // Start Server
 // ============
 
-app.listen(port, (req, res) => {
-  console.log(`Server is running at port ${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
